b4dfae3e902cc8aacfac3b9cad1996088e5d6e30
phwrscr1pt <pkhyomp@gmail.com>
2025-08-02 02:11:10 +0700

    foruth commit: finish condition

--- atbash_decrpyt.py
+++ atbash_decrpyt.py
@@ -2,4 +2,11 @@ def atbash_decrypt(ciphertext):
     result = ""
     for char in ciphertext:
         if char.isalpha():
-            pass
+            if char.islower():
+                result += chr(ord('z') - (ord(char) - ord('a')))
+            else :
+                result += chr(ord('Z' - (ord(char) - ord('A'))))
+        else :
+            result += char
+    
+    return result
